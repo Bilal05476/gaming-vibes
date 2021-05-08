42c5bbe99fa4499db76e7325aee316569c6bc16f
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-05-08 14:21:44 +0500

    updates

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
@@ -8,13 +8,21 @@ export default function Navbar() {
         <Image src="/rupee.png" width={50} height={48} />
         <h1>Gaming Vibes</h1>
         <ul>
-          <li><Link href="/"><a>Home</a></Link></li>
-          <li><Link href="/guides"><a>Guides</a></Link></li>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/guides">
+              <a>Guides</a>
+            </Link>
+          </li>
         </ul>
       </nav>
       <div className="banner">
         <Image src="/banner.png" width={966} height={276} />
       </div>
     </div>
-  )
+  );
 }
--- stores/authContext.js
+++ stores/authContext.js
@@ -1,4 +1,5 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import netlifyIdentity from "netlify-identity-widget";
 
 const AuthContext = createContext({
   user: null,
@@ -6,3 +7,14 @@ const AuthContext = createContext({
   logout: () => {},
   authReady: false,
 });
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    //init netlify identity
+    netlifyIdentity.init();
+  }, []);
+  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+};
+
+export default AuthContext;
